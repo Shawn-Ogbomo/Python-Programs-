8345011a07f522f3c197047b175af4b7b7bdfcec
Shawn Ogbomo <44599293+Shawn-Ogbomo@users.noreply.github.com>
2020-02-18 01:58:18 -0500

    Delete list_comprehension_example.py

--- list_comprehension_example.py
+++
@@ -1,5 +0,0 @@
-#This program will store a  list of numbers ranging from 1 - 10.
-#and then square each value in the list.
-#uses one line of code as opposed to 3 - 4 lines.
-cubed = [value**3 for value in range(0,30)]
-print(cubed)
