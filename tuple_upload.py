20c84b46d2180d524e11fe2fffb3fd01b85868af
Shawn Ogbomo <44599293+Shawn-Ogbomo@users.noreply.github.com>
2020-02-20 23:31:07 -0500

    Add files via upload

---
+++ tuple_upload.py
@@ -0,0 +1,9 @@
+#This program will store five foods to a tuple.
+#The program will then print out the elements in the touple using a for loop.
+#The tuple will then be redefined with modifications to the last two entries, thus printing out the results.
+five_basic_foods = ('rice','chicken','potatoes','burgers','fries')
+for five_foods in five_basic_foods:
+	print(five_foods,"\n")
+five_basic_foods = ('rice', 'chicken','potatoes','muffins','salad')
+for changes_in_tuple in five_basic_foods:
+	print(changes_in_tuple,"\n") 
