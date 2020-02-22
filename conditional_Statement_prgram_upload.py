76741e51484fce1ac698d8fe0661fc6ae8b749ac
Shawn Ogbomo <44599293+Shawn-Ogbomo@users.noreply.github.com>
2020-02-21 23:49:42 -0500

    Update conditional_Statement_prgram_upload.py

--- conditional_Statement_prgram_upload.py
+++ conditional_Statement_prgram_upload.py
@@ -1,6 +1,6 @@
 #This program will store various foods inside a list named foods.
-#Then it will loop though the foods in the list in search for the entry 'fries'
-#Once the entry 'fries' is found, the varuable message capitalized and then printed. 
+#Then it will loop though the foods in the list in search of the entry 'fries'
+#Once the entry 'fries' is found, the variable 'message' is capitalized and then printed. 
 foods = ['pizza','burgers','fries']
 message = "are my favorite food!"
 for food in foods:
